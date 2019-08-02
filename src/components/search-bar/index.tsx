0170cb9a193722a42eb15c6aaa9f6bc022085ae6
zhuliminl <zhuliminl@gmail.com>
2019-08-02 17:38:55 +0800

    搜索栏初始化

--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -1,9 +1,10 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text, Image } from '@tarojs/components'
+import { View, Text, Input, Image } from '@tarojs/components'
 import './index.scss'
 
 import icon_search_red from '@/assets/icon/search-red.png'
 import icon_back from '@/assets/icon/back.png'
+import { device } from '@/utils/device';
 
 interface SearchBarInterface {
   placeholder: string;
@@ -15,13 +16,30 @@ export default class SearchBar extends Component<SearchBarInterface, {}> {
   componentDidMount = () => {
   }
 
+  handleOnChange = event => {
+
+  }
+
   render() {
+    const inputStyle: any = {}
+    if(device.isAndroid()) {
+      inputStyle.height = 40
+    }
     return (
       <View className="search-bar-comp">
-        <Image className='search-bar-back-img' src={icon_back}/>
+        <View className='search-bar-back-img-wrap' onClick={() => {Taro.navigateBack()}}>
+          <Image className='search-bar-back-img' src={icon_back}/>
+        </View>
         <View className='search-bar-wrap'>
           <Image className='search-bar-img' src={icon_search_red} />
-          <Text className='search-bar-title-thin'>{this.props.placeholder}</Text>
+          <Input 
+            type='text'
+            placeholder={this.props.placeholder}
+            className='search-bar-input'
+            focus
+            onInput={this.handleOnChange.bind(this)}
+            style={inputStyle}
+            />
           <Text className='search-bar-red-btn'>搜券</Text>
         </View>
       </View>
